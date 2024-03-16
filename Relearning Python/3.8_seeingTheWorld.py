7e6a0219b239dd79cf377625bae8ea4f6bed3995
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-03-16 05:16:19 -0400

    new problem

--- Relearning Python/3.8_seeingTheWorld.py
+++ Relearning Python/3.8_seeingTheWorld.py
@@ -5,4 +5,15 @@
 # Use sorted() to print list in reverse alphabetical order without modifying the list
 # Use reverse() to change the order of the list
 # USe sort() to change the order of the list permanently
-# Use
+# Use sort() to change your list so it's stored in reverse alphabetical order
+
+locations = ['Paris', 'London', 'New York', 'Tokyo', 'Sydney']
+print(locations)
+print(sorted(locations)) # Temporarily sorted
+print(sorted(locations, reverse=True)) # Temporarily sorted in reverse
+locations.reverse() # Permanently reversed
+print(locations) # Permanently reversed
+locations.sort() # Permanently sorted
+print(locations) # Permanently sorted
+locations.sort(reverse=True) # Permanently sorted in reverse
+print(locations) # Permanently sorted in reverse
---
+++ Relearning Python/5.1_conditionalTest.py
@@ -0,0 +1,2 @@
+# Write a series of conditional tests.
+# Print a statement descriving each test and your prediciton for the result of each test.
