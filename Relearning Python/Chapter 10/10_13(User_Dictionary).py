bea7e0c512df44bbae4fe12fc5daf780b6672f13
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-05-03 20:32:43 -0400

    fix some issues

--- Relearning Python/Chapter 10/10_13(User_Dictionary).py
+++ Relearning Python/Chapter 10/10_13(User_Dictionary).py
@@ -1,17 +1,8 @@
-"""
-The remember_me.py example only stores one piece of
-information, the username. Expand this example by asking for two more pieces
-of information about the user, then store all the information you collect in a
-dictionary. Write this dictionary to a file using json.dumps(), and read it back
-in using json.loads(). Print a summary showing exactly what your program
-remembers about the user.
-"""
-
 from pathlib import Path
 import json
 
 def get_stored_user_info(path):
-    """Get stored username if available."""
+    """Get stored user information if available."""
     if path.exists():
         contents = path.read_text()
         user_info = json.loads(contents)
@@ -20,15 +11,11 @@ def get_stored_user_info(path):
         return None
 
 def get_new_user_info(path):
-    """Prompt for a new user information."""
-    username = input("What is your name?")
-    hobby = input(f"What's your hobby {username}? ")
-    interest = input(f"What's your interest {username}? ")
-    user_info = {
-        'username':username,
-        'Hobby':hobby,
-        'Interest':interest 
-    }
+    """Prompt for new user information."""
+    username = input("What is your name? ")
+    hobby = input(f"What's your hobby, {username}? ")
+    interest = input(f"What's your interest, {username}? ")
+    user_info = {'username': username, 'Hobby': hobby, 'Interest': interest}
     contents = json.dumps(user_info)
     path.write_text(contents)
     return user_info
@@ -42,16 +29,20 @@ def greet_user():
     else:
         user_info = get_new_user_info(path)
         print(f"We'll remember you when you come back, {user_info['username']}!")
-        
+
 def show_user_info():
-    """Shpw stored user info"""
+    """Show the stored user information."""
     path = Path('username.json')
     user_info = get_stored_user_info(path)
     if user_info:
+        print("\nUser Information:")
         for key, value in user_info.items():
-            print(f'{key}, {value}')
-        else:
-            print("No Info")
-            
+            print(f"{key}: {value}")
+    else:
+        print("No user information found.")
+
+# Greet the user and collect information
 greet_user()
+
+# Show the stored user information
 show_user_info()
