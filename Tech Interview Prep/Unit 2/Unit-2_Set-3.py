5ed16796ebfcafb68b083db52095796b542f5bcd
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-06-12 00:44:24 -0400

    6/12/2024

--- Tech Interview Prep/Unit 2/Unit-2_Set-3.py
+++ Tech Interview Prep/Unit 2/Unit-2_Set-3.py
@@ -16,3 +16,27 @@ def peak_index_in_mountain_list(mountain_lst) :
 mountain_lst = [0,3,8,0]
 peak = peak_index_in_mountain_list(mountain_lst)
 print(peak)
+print()
+
+"""
+Write a function build_inventory() that takes in a list of product_names and a corresponding list of product_prices as parameters. The function returns a dictionary representing the inventory of a small store. Each product name in product_names should be a key in the dictionary and the corresponding value should be the item price.
+
+product_names[i] should be paired with product_prices[i] in the dictionary where 0 <= i <= len(product_names). You may assume that product_names and product_prices are the same length.
+"""
+def build_inventory(product_names, product_prices) :
+    output = {}
+    
+    for i in range(len(product_names)) :
+        # Set output key as product_names items
+        # Set output value as product_prices items
+        output[product_names[i]] = product_prices[i]
+    return output
+
+product_names = ["Apple", "Banana", "Orange"]
+product_prices = [0.99, 0.50, 0.75]
+print(build_inventory(product_names, product_prices))
+print()
+
+"""
+
+"""
