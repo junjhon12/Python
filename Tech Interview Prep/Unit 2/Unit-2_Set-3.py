254e1105b5513671c35e120587f1e411f758509d
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-06-13 12:25:00 -0400

    6/

--- Tech Interview Prep/Unit 2/Unit-2_Set-3.py
+++ Tech Interview Prep/Unit 2/Unit-2_Set-3.py
@@ -16,3 +16,37 @@ def peak_index_in_mountain_list(mountain_lst) :
 mountain_lst = [0,3,8,0]
 peak = peak_index_in_mountain_list(mountain_lst)
 print(peak)
+print()
+
+"""
+Write a function build_inventory() that takes in a list of product_names and a corresponding list of product_prices as parameters. The function returns a dictionary representing the inventory of a small store. Each product name in product_names should be a key in the dictionary and the corresponding value should be the item price.
+
+product_names[i] should be paired with product_prices[i] in the dictionary where 0 <= i <= len(product_names). You may assume that product_names and product_prices are the same length.
+"""
+def build_inventory(product_names, product_prices) :
+    output = {}
+    # Iterate at index 0
+    for i in range(len(product_names)):
+        # Set key as product_names item and value is product_prices item
+        output[product_names[i]] = product_prices[i]
+    return output
+    
+product_names = ["Apple", "Banana", "Orange"]
+product_prices = [0.99, 0.50, 0.75]
+print(build_inventory(product_names, product_prices))
+print()
+
+"""
+Write a function update_or_warn() that takes in a dictionary records, a key item, and a new value update_value as parameters. The function updates the value of item in records with update_value if item exists. If item does not exist, it should print "<item> not found!".
+"""
+def update_or_warn(records, item, update_value) :
+    for i in records :
+        if item in records :
+            records[i] = update_value
+            print(records)
+        else :
+            print(item)
+    
+records = {"apple": 1, "banana": 2, "orange": 3}
+update_or_warn(records, "banana", 5)
+update_or_warn(records, "grape", 4)
